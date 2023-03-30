ee50721028b013b32fc27821b3544ba09a7f79fe
Hardik Sharma <104981324+hardiksharma11@users.noreply.github.com>
2023-03-30 17:57:12 +0530

    Added dynamic API

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -8,7 +8,7 @@ const Login = (props) => {
 
     const login = async (email,password) => {
 
-        const response = await fetch(`http://localhost:5000/api/auth/login`, {
+        const response = await fetch(`https://inotebooks.onrender.com/api/auth/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
