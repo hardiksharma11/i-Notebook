b3ea9ffe22b840e2b7e5a0fdcb7dfb6ce56563c2
Hardik Sharma <104981324+hardiksharma11@users.noreply.github.com>
2023-03-30 17:58:11 +0530

    Added Dynamic API

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -7,7 +7,7 @@ const Signup = (props) => {
 
   const signup = async (name, email, password) => {
 
-    const response = await fetch(`http://localhost:5000/api/auth/createuser`, {
+    const response = await fetch(`https://inotebooks.onrender.com/api/auth/createuser`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
